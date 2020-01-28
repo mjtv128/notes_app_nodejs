252bbf54a1a0c7a43ecd3db070fb5a1f637a3133
Michelle Van <mjtvan@gmail.com>
2020-01-28 14:50:24 +0000

    prevents duplication when adding notes

--- notes.js
+++ notes.js
@@ -6,14 +6,25 @@ const getNotes = function() {
 
 const addNote = function(title, body) {
     const notes = loadNotes()
-
-    notes.push({
-        title: title, 
-        body: body
+    const duplicateNotes = notes.filter(note => {
+        return note.title === title
     })
+     
+    if (duplicateNotes.length === 0) {
+         notes.push({
+           title: title,
+           body: body
+         });
 
-    saveNotes(notes)
+         saveNotes(notes);
+         console.log("New notes added")
+    } else {
+        console.log("Note title taken!")
+    }
+   
 }
+
+
 
 const saveNotes = function(notes){
     const dataJSON = JSON.stringify(notes)
